5a6831a7128a4d3548903167d3fc4aca986fdf21
nizam <nizamclash@gmail.com>
2023-01-30 17:15:52 +0530

    added on click event of add Button to add the task.

--- app/src/main/java/com/example/todoo/MainActivity.java
+++ app/src/main/java/com/example/todoo/MainActivity.java
@@ -7,9 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                tv1 = new TextView(MainActivity.this);
+                tv1.setText("Hello World");
+                linearLayout.addView(tv1);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int)LinearLayout.LayoutParams.WRAP_CONTENT,(int)LinearLayout.LayoutParams.WRAP_CONTENT);
+                tv1.setTextSize(34);
+                linearLayout.setLayoutParams(params);
 
             }
         });
