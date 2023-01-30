1bde78f4b1e62b0fe631d2b16b037eedd4ee6f9e
nizam <nizamclash@gmail.com>
2023-01-30 17:25:58 +0530

    Added Edit text to take input of Task needed to be done. And also passed its value to TextView and setted it on screen at run time.

--- app/src/main/java/com/example/todoo/MainActivity.java
+++ app/src/main/java/com/example/todoo/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.QuickContactBadge;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv1;
+    EditText edtxt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout linearLayout = findViewById(R.id.linearLayout);
         Button btnAdd = findViewById(R.id.addButton);
+        edtxt1 = findViewById(R.id.edtxt1);
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 tv1 = new TextView(MainActivity.this);
-                tv1.setText("Hello World");
+                tv1.setText(edtxt1.getText());
                 linearLayout.addView(tv1);
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int)LinearLayout.LayoutParams.WRAP_CONTENT,(int)LinearLayout.LayoutParams.WRAP_CONTENT);
                 tv1.setTextSize(34);
